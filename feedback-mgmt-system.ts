87a0773dac13844607478c2abd4cc0d1cf977111
Jundullah Ismail <jundullah.ismail@terang.asia>
2018-12-07 17:29:55 +0800

    first draft

---
+++ feedback-mgmt-system.ts
@@ -0,0 +1,77 @@
+interface Event {
+    name: string;
+    eventDate: Date;
+    venue: string;
+    attendees: Attendee[];
+    sponsorships: Sponsorship[];
+    organizer: Organizer[];
+}
+
+interface Organizer {
+    companyName: string;
+    committee: Committee[];
+}
+
+interface Committee {
+    name: string;
+    role: string;
+    age: number;
+    identityNo: string;
+    gender: Gender;
+}
+
+interface Sponsorship {
+    sponsor: Sponsor;
+    type: SponsorshipType;
+    amountSponsored: number;
+}
+
+enum SponsorshipType {
+    PLATINUM,
+    GOLD,
+    SILVER,
+}
+
+interface Sponsor {
+    companyName: string;
+    picName: string;
+}
+
+interface Attendee {
+    name: string;
+    profession: string;
+    age: number;
+    identityNo: string;
+    gender: Gender;
+    feedback?: Feedback;
+}
+
+interface Feedback {
+    comment: string;
+    rating: number;
+}
+
+enum Gender {
+    MALE,
+    FEMALE,
+}
+
+interface NonVip extends Attendee {
+    role: Role;
+
+}
+
+enum Role {
+    PARTICIPANT,
+    PUBLIC,
+}
+
+interface Vip extends Attendee {
+    title: Title;
+}
+
+enum Title {
+    DATUK,
+    DATIN,
+    PROFESSOR,
+}
